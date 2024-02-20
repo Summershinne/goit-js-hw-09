d1361e065c4453286d1f43e9d35e602414169310
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-02-20 22:38:16 +0100

    home work

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,30 @@
-console.log("Form")
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+function readFormData(form) {
+    const inputEmail = form.email.value.trim('');
+    const message = form.message.value.trim('');
+    return {
+        inputEmail,
+        message
+    }
+}
+form.addEventListener('input', (event) => {
+    event.preventDefault();
+    const data = readFormData(event.currentTarget);
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem(STORAGE_KEY, jsonData);
+}
+)
+const rowData = localStorage.getItem(STORAGE_KEY)
+if (rowData) {
+    const data = JSON.parse(rowData);
+    form.email.value = data.email;
+    form.message.value = data.message;
+}
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    console.log(event.email);
+    console.log(event.message);
+})
